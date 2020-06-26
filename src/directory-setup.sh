c5ae02a9c6de93e2d6dc4888f7a847975bc2258f
Yufei Cui <yc89@risotto.cs.rice.edu>
2020-06-26 00:56:47 -0500

    modified:   src/directory-setup.sh

--- src/directory-setup.sh
+++ src/directory-setup.sh
@@ -2,19 +2,13 @@
 # This script sets up the directory structure
 # relative path is based on src directory, where the location of this script is
 src_dir=$(dirname $0)
-data_type=(data raw)
+data_type=(data raw processed)
 database=(GEO TCGA ENCODE)
-assay_type=(microarray sequencing)
 for i in "${data_type[@]}"; do
 	mkdir -p $src_dir/../$i
 		for j in "${database[@]}"; do
 			mkdir -p $src_dir/../$i/$j
 		done
-done
-for j in "${database[@]}"; do
-	for z in "${assay_type[@]}"; do
-		 mkdir -p $src_dir/../processed/$j/$z
-	 done
 done
 mkdir -p  $src_dir/../annotation
 	
