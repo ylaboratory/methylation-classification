0e9e13540ef3d59b0d3391c6e4d1be95d47772e7
Yufei Cui <yc89@risotto.cs.rice.edu>
2020-07-03 15:15:36 -0500

    renamed:    src/setup.sh -> src/build-dataset-microarray.sh 	new file:   src/initial_setup.sh

--- src/build-dataset-microarray.sh
+++ src/build-dataset-microarray.sh
@@ -1,23 +1,6 @@
 #!/bin/bash
-# This script sets up the directory structure
 # relative path is based on src directory, where the location of this script is
 src_dir=$(dirname $0)
-data_type=(data raw processed)
-database=(GEO TCGA ENCODE)
-for i in "${data_type[@]}"; do
-	for j in "${database[@]}"; do
-		mkdir -p -v $src_dir/../$i/$j
-	done
-done
-mkdir -p -v $src_dir/../annotation
-
-# download hg19 to hg38 chain files for liftover
-annotation=$src_dir/../annotation/hg19ToHg38.over.chain
-if [[ ! -f "$annotation" ]]
-then
-	wget 'ftp://hgdownload.cse.ucsc.edu/goldenPath/hg19/liftOver/hg19ToHg38.over.chain.gz' -O $src_dir/../annotation/hg19ToHg38.over.chain.gz 
-	gunzip $src_dir/../annotation/hg19ToHg38.over.chain.gz
-fi
 
 # read in the text file containing accession numbers of datasets to be processed
 usage() {
