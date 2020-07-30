6ea4ba456fba880e05283b7422350c581a9b267c
Yufei Cui <yc89@risotto.cs.rice.edu>
2020-07-30 10:16:44 -0500

    Specify paired/single end data for methylation extraction Added paired/single end option in bismark_methylation_extraction, also treated each technical replicate as individual sample and extracted methylation percentage (methylation status couble be averaged in the future)

--- src/processing-geo-sequencing-RRBS.sh
+++ src/processing-geo-sequencing-RRBS.sh
@@ -18,23 +18,21 @@ src_dir=$(dirname $0)
 # get SRR accession from GSE and GSM
 SRR=( $(esearch -db sra -query $GSM | efetch -format docsum | xtract -pattern DocumentSummary -element Run@acc) )
 echo ${SRR[@]}
+# for each RRBS sample, perform adaptor trimming, alignment and extract the methylation percentage
 for i in "${SRR[@]}"
 do
 	filename='.fastq'
 	filename_align='_trimmed.fq'
-	count=$( ls -1q $src_dir/../raw/GEO/$GSE/$i??$filename | wc -l )
+	count=$( ls -1q $src_dir/../raw/GEO/$GSE/$i*$filename | wc -l )
 	echo $count
 	if  [ $count -gt 1 ]; then
 		trim_galore --paired --rrbs -j 6 -o $src_dir/../raw/GEO/$GSE  $src_dir/../raw/GEO/$GSE/$i"_1"$filename $src_dir/../raw/GEO/$GSE/$i"_2"$filename
 		bismark -o $src_dir/../processed/GEO/$GSE --multicore $core_count --temp_dir $src_dir/../processed/GEO/$GSE --genome $src_dir/../annotation -1 $src_dir/../raw/GEO/$GSE/$i"_1_val_1.fq" -2 $src_dir/../raw/GEO/$GSE/$i"_2_val_2.fq"
+		bismark_methylation_extractor -p --no_overlap --comprehensive --multicore $core_count --zero_based --bedGraph --cutoff 20 -o $src_dir/../data/GEO/$GSE $src_dir/../processed/GEO/$GSE/$i*".bam"
 	else
 		trim_galore --rrbs -j 6 -o $src_dir/../raw/GEO/$GSE  $src_dir/../raw/GEO/$GSE/$i$filename
 		bismark -o $src_dir/../processed/GEO/$GSE --multicore $core_count --temp_dir $src_dir/../processed/GEO/$GSE --genome $src_dir/../annotation $src_dir/../raw/GEO/$GSE/$i$filename_align
+		bismark_methylation_extractor -s --multicore $core_count --zero_based --bedGraph --cutoff 20 -o $src_dir/../data/GEO/$GSE $src_dir/../processed/GEO/$GSE/$i*".bam"
 	fi
 	echo "finished alignment"
 done
-filename_extract=".bam"
-filename_list=( "${SRR[@]/%/*$filename_extract}" )
-filename_prefix="$src_dir/../processed/GEO/$GSE/"
-filename_list=( "${filename_list[@]/#/$filename_prefix}" )
-bismark_methylation_extractor --comprehensive --multicore $core_count --zero_based --bedGraph --cutoff 20 -o $src_dir/../data/GEO/$GSE $filename_list
